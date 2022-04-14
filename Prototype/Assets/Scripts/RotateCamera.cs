76de5dfb9fe9c2d42dc3942442c99465b823a326
vinsdragonis <zrexteam128@gmail.com>
2022-04-15 00:40:44 +0530

    Update RotateCamera.cs

--- Prototype/Assets/Scripts/RotateCamera.cs
+++ Prototype/Assets/Scripts/RotateCamera.cs
@@ -6,7 +6,7 @@ public class RotateCamera : MonoBehaviour
 {
     [SerializeField] Camera cameraObj;
     [SerializeField] GameObject myGameObj;
-    [SerializeField] float speed = 2f;
+    [SerializeField] float speed = 4f;
     [SerializeField] new GameObject gameObject;
     bool isRotated;
 
@@ -32,7 +32,7 @@ public class RotateCamera : MonoBehaviour
             {
                 cameraObj.transform.RotateAround(myGameObj.transform.position,
                                                 cameraObj.transform.up,
-                                                -Input.GetAxis("Mouse X") * speed);
+                                                Input.GetAxis("Mouse X") * speed);
 
                 // cameraObj.transform.RotateAround(myGameObj.transform.position,
                 //                                 cameraObj.transform.right,
