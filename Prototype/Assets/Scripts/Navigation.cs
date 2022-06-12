25e7a381cfe60359bad05d1cc758f4e7486e153a
destathe <derek.cs19@bmsce.ac.in>
2022-06-12 21:52:15 +0530

    adding instruction step 1

--- Prototype/Assets/Scripts/Navigation.cs
+++ Prototype/Assets/Scripts/Navigation.cs
@@ -3,31 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
 
+
 public class Navigation : MonoBehaviour
 {
     public UIDocument Document;
-    int index=0;
+    public GameObject mainCamera;
     
     // Start is called before the first frame update
     void Awake() {
-        
+        mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
+        rotateToPosition rpt = mainCamera.GetComponent<rotateToPosition>();
+
         var root = Document.rootVisualElement;
         var nextBT = root.Q<Button>("Nav_Next");
-        var backBT = root.Q<Button>("Back_Next");
+        var backBT = root.Q<Button>("Nav_Prev");
         var instruction = root.Q<Label>("Instruction");
         if(nextBT != null)
         {
             nextBT.clicked += () => {
-                index+=1;
-                instruction.text = index.ToString();
+                rpt.index+=1;
+                instruction.text = rpt.index.ToString();
+            };
+        }
+        if(backBT != null)
+        {
+            backBT.clicked += () => {
+                rpt.index-=1;
+                instruction.text = rpt.index.ToString();
             };
         }
     }
-    // void Start()
-    // {
-        
-    // }
-
-    // // Update is called once per frame
+    
     
 }
